bfe4086fcf11bb2ce4e601faf5843dde0d234f1d
joaorenes <joaorenes.engenharia@gmail.com>
2023-12-03 20:00:15 -0300

    ajustes

---
+++ aula-2711/script.js
@@ -0,0 +1,15 @@
+function helloFunc(){
+    alert("Alô Mundo!!!");
+}
+
+function formValidation() {
+   console.log(document.getElementById("meuid").value); //= a.value.toUpperCase());
+} 
+
+const obj = {
+    nome: "Elisaudo",
+    endereco: "Rua XYZ",
+    numero: 34
+}
+
+console.log(typeof obj);
